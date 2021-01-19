c568273bb4f435befa7cdcc7717ee2c603cd23b2
swatisignature <swatimishra.puri@gmail.com>
2021-01-18 21:25:40 -0800

    Linked List

--- Data_Structures/LinkedList.py
+++ Data_Structures/LinkedList.py
@@ -1,12 +1,15 @@
+# Definition of Node class for Linked List
 class ListNode:
     def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
+# Definition of Linked List class
 class LinkedList:
     def __init__(self):
         self.head = None
 
+    # insert at the beginning, time complexity: O(n)
     def insert_at_begin(self,val):
         new_node = ListNode(val)
         if self.head is None:
@@ -15,6 +18,7 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
 
+    # insert at end, time complexity: O(n)
     def insert_at_end(self,val):
         new_node = ListNode(val)
         if self.head is None:
@@ -27,10 +31,12 @@ class LinkedList:
                 curr_node = curr_node.next
             curr_node.next = new_node
 
+    # insert multiple values in the linked list, time complexity: O(n)
     def insert_multiple(self,add_list):
         for each in add_list:
             self.insert_at_end(each)
 
+    # insert value at the given index in the linked list, time complexity: O(n)
     def insert_at(self,ind,value):
         if ind < 0 or ind > self.get_length():
             raise Exception("Invalid Index")
@@ -50,9 +56,7 @@ class LinkedList:
             count += 1
         return
 
-
-
-
+    # Get the size of the linked list, time complexity: O(n)
     def get_length(self):
         if self.head is None:
             print("The List is Empty!!!")
@@ -64,7 +68,7 @@ class LinkedList:
                 curr_node = curr_node.next
         return size
 
-
+    # Print values in the linked list, time complexity: O(n)
     def print_list(self):
         if self.head is None:
             print("LinkedList is empty")
@@ -77,12 +81,14 @@ class LinkedList:
                 curr_node = curr_node.next
             print(llist_str)
 
+    # remove value at the start in the linked list, time complexity: O(1)
     def remove_at_begin(self):
         if self.head is None:
             return
         else:
             self.head = self.head.next
 
+    # remove value at the end in the linked list, time complexity: O(n)
     def remove_at_end(self):
         if self.head is None:
             return
@@ -92,6 +98,7 @@ class LinkedList:
                 curr_node = curr_node.next
             curr_node.next = None
 
+    # remove value at the given index in the linked list, time complexity: O(n)
     def remove_at(self,ind):
         if ind < 0 or ind > self.get_length():
             raise Exception("Out of Bound")
@@ -113,6 +120,7 @@ class LinkedList:
                 count +=1
                 curr_node = curr_node.next
 
+    # insert value after a given value in the linked list, time complexity: O(n)
     def insert_after_value(self,val,new_val):
         if self.head is None:
             return
@@ -124,6 +132,7 @@ class LinkedList:
                     curr_node.next = new_node
                 curr_node = curr_node.next
 
+    # remove value after a given value in the linked list, time complexity: O(n)
     def remove_by_value(self,val):
         if self.head is None:
             return
